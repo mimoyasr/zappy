d1085fc0341059c49af4689bca7a8aa7beca72f0
mimoyasr <amryasserhamza@gmail.com>
2018-05-15 20:49:02 +0200

    update app.js add mongoose

--- app.js
+++ app.js
@@ -3,9 +3,16 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+mongoose.connect('mongodb://localhost:27017/zappy');
+mongoose.connection.once('open', () => {
+    console.log('connected to mongo')
+})
+mongoose.connection.on('error', (err) => {
+    console.log('error connecting to mongo', err)
+})
+
 
 var app = express();
 
@@ -24,18 +31,18 @@ app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
